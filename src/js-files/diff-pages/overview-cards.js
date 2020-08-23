9184a71fed5af93a51c51e688c8d2d84dd24c720
danblevins <danblevins@Dans-MacBook-Pro.local>
2020-08-23 14:50:58 -0700

    Updated Text

--- src/js-files/diff-pages/overview-cards.js
+++ src/js-files/diff-pages/overview-cards.js
@@ -2,21 +2,21 @@ const overCards = [
     {
         key: 1,
         img: "https://ichef.bbci.co.uk/news/410/cpsprodpb/37B5/production/_89716241_thinkstockphotos-523060154.jpg",
-        title: "The Dice Machine",
-        content: "This is a game where you don't when you win, and you don't know when you lose its a total game of deception.",
+        title: "View our Github Repository",
+        content: "Our open-source repository is created and supported by the Economics Advisory Board. Contributions are always appreciated.",
         btnSrc: "/SQL"
     },
     {
         key: 2,
         img: "https://media.nesta.org.uk/images/Predictions-2019_Twitter_02.width-1200.png",
-        title: "LightBulb Bingo",
-        content: "A new form of Bingo that is played with lightbulbs. Lots of glass broken and lots of winners.",
+        title: "Learn about Github/ Git",
+        content: "Want to learn more about Github and Git? We have plenty of resources for you.",
         btnSrc: "/github"
     },
     {
         key: 3,
         img: "https://www.randomlists.com/img/world-city-maps/belgrade.webp",
-        title: "Google Maps Risk",
+        title: "REMOVE?",
         content: "Take over the world with this Google maps inspired version of Risk. Conquer the world!",
         btnSrc: "/python"
     }
