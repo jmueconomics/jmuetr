891d1b607fc125f8e9805f633d0ebb38acfd9616
Damon Roberts <66637938+damonspencerroberts@users.noreply.github.com>
2020-08-26 01:29:33 -0400

    trying to push to heroku

--- src/components/navbar/jmu-nav.jsx
+++ src/components/navbar/jmu-nav.jsx
@@ -13,11 +13,7 @@ return <Navbar collapseOnSelect expand="lg" className = "color-nav" variant = "d
     <Navbar.Toggle aria-controls="basic-navbar-nav" />
   <Navbar.Collapse id="basic-navbar-nav">
     <Nav className="ml-auto">
-<<<<<<< HEAD
       <Nav.Link href="/"><i class="home fas fa-home"></i></Nav.Link>
-=======
-      <Nav.Link href="/">Home</Nav.Link>
->>>>>>> 0d2342dd4ced0ff8c23060d681196a6ba2c78942
       <Nav.Link href = {"/" + _.lowerCase("Github")}>Github</Nav.Link>
       <HoverDropdown />
       <Nav.Link href={"/" + _.lowerCase("About")}>About</Nav.Link>
