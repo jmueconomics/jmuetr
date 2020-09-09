15f5819589a3de11ea5412a1d1a49c9cd53fe169
Damon Roberts <66637938+damonspencerroberts@users.noreply.github.com>
2020-09-09 16:11:08 -0400

    Added head top cards

--- src/components/created-functions/card.jsx
+++ src/components/created-functions/card.jsx
@@ -1,10 +1,11 @@
+/* eslint-disable react/jsx-no-target-blank */
 import React from "react";
 import {Card} from "react-bootstrap";
 
 
 //Using React bootstrap
 const createCard = (props) => 
-    <a className = "cardA" href={props.btnSrc}>
+    <a className = "cardA" href={props.btnSrc} target = "_blank">
       <Card className = "each-card" >
         <div>
           <Card.Img variant="top" src={props.img} className = {props.imgClass} />
--- src/js-files/diff-pages/head-top-card-content.js
+++ src/js-files/diff-pages/head-top-card-content.js
@@ -1,25 +1,30 @@
+import EconBoard from "../../components/img/home/econ-board.png";
+import GitStarted from "../../components/img/home/git-started.png";
+import GitLab from "../../components/img/home/git-lab.png";
+
+
 const headTop = [
     {
         key: 123435,
-        img: "https://ichef.bbci.co.uk/news/410/cpsprodpb/37B5/production/_89716241_thinkstockphotos-523060154.jpg",
-        title: "The Dice Machine",
-        content: "This is a game where you don't when you win, and you don't know when you lose its a total game of deception.",
-        btnSrc: "/SQL"
+        img: EconBoard,
+        title: "View our Github Repository",
+        content: "Our open-source repository is created and supported by the Economics Advisory Board. Contributions are always appreciated.",
+        btnSrc: "https://github.com/jmueconomicsalumni"
         
     },
     {
-        key: 234235443,
-        img: "https://media.nesta.org.uk/images/Predictions-2019_Twitter_02.width-1200.png",
-        title: "LightBulb Bingo",
-        content: "A new form of Bingo that is played with lightbulbs. Lots of glass broken and lots of winners.",
-        btnSrc: "/github"
+        key: 3342234343454,
+        img: GitLab,
+        title: "Github Learning Lab",
+        content: "With GitHub Learning Lab, grow your skills by completing fun, realistic projects. Get advice and helpful feedback from our friendly Learning Lab bot.",
+        btnSrc: "https://lab.github.com/"
     },
     {
-        key: 3342234343454,
-        img: "https://www.randomlists.com/img/world-city-maps/belgrade.webp",
-        title: "Google Maps Risk",
-        content: "Take over the world with this Google maps inspired version of Risk. Conquer the world!",
-        btnSrc: "/python"
+        key: 234235443,
+        img: GitStarted,
+        title: "Git Started with Github",
+        content: "A short course designed to jump right into showing how Git and GitHub work together. You can expect to learn the minimum needed to start using Git in about 30 minutes.",
+        btnSrc: "https://www.udemy.com/course/git-started-with-github/?LSNPUBID=JVFxdTr9V80&ranEAID=JVFxdTr9V80&ranMID=39197&ranSiteID=JVFxdTr9V80-oxsazaPpvwovMRAUJ4oofw&utm_medium=udemyads&utm_source=aff-campaign"
     }
 ];
 
