3017ef144117848c86c84ea9c2ea8151d709853b
Damon Roberts <66637938+damonspencerroberts@users.noreply.github.com>
2020-09-09 17:21:32 -0400

    Added github page cards

--- src/components/pages/github-page/github-top-cards.jsx
+++ src/components/pages/github-page/github-top-cards.jsx
@@ -1,17 +1,17 @@
 import React from "react";
-import array from "../../../js-files/diff-pages/head-top-card-content";
+import {GitResources, GitTutorials} from "../../../js-files/diff-pages/github-resources";
 import Cards from "../../created-functions/three-cards";
 
 const GithubTopCards = () => 
     <div className = "git-tut-cards">
         <hr/>
-        {Cards("git-tutorial-title", "git-tutorials-top-cards", "Github Tutorials", array, 4, 6)}
+        {Cards("git-tutorial-title", "git-tutorials-top-cards", "Github Tutorials", GitResources, 4, 6)}
     </div>
 
 const GithubBottomCards = () => 
     <div className = "git-tut-cards">
         <hr/>
-        {Cards("git-resource-title", "git-tutorials-bottom-cards", "Github Resources", array, 4, 6)}
+        {Cards("git-resource-title", "git-tutorials-bottom-cards", "Github Resources", GitTutorials, 4, 6)}
     </div>
 
 export { GithubTopCards, GithubBottomCards };
---
+++ src/js-files/diff-pages/github-resources.js
@@ -0,0 +1,60 @@
+import Command from "../../components/img/github/command.png";
+import GitLab from "../../components/img/github/git-learn.png";
+import GitLogo from "../../components/img/github/git-logo.png";
+import Hubspot from "../../components/img/github/hubspot.png";
+import Magic from "../../components/img/github/magic.png";
+import Version from "../../components/img/github/version.png";
+
+
+
+const GitResources = [
+    {
+        key: 123435,
+        img: GitLab,
+        title: "Github Lab",
+        content: "With GitHub Learning Lab, grow your skills by completing fun, realistic projects. Get advice and helpful feedback from our friendly Learning Lab bot.",
+        btnSrc: "https://lab.github.com/"
+        
+    },
+    {
+        key: 3342234343454,
+        img: Command,
+        title: "Command Line Essentials: Git Bash",
+        content: "A step-by-step guide for Windows users unfamiliar with Bash or the terminal environment. Essential for a career in Data Science or anything in the Tech industry.",
+        btnSrc: "https://www.udemy.com/course/git-bash/?LSNPUBID=JVFxdTr9V80&ranEAID=JVFxdTr9V80&ranMID=39197&ranSiteID=JVFxdTr9V80-er2IXn6JypzfEugwLL4cgg&utm_medium=udemyads&utm_source=aff-campaign"
+    },
+    {
+        key: 234235443,
+        img: Version,
+        title: "Version Control with Git",
+        content: "The Version Control with Git course provides  you with a solid, hands-on foundation for understanding the Git version control system. Another essential tool to grasp.",
+        btnSrc: "https://www.coursera.org/learn/version-control-with-git?ranMID=40328&ranEAID=JVFxdTr9V80&ranSiteID=JVFxdTr9V80-alfDKbQG6gGANAxdHRWH4w&siteID=JVFxdTr9V80-alfDKbQG6gGANAxdHRWH4w&utm_content=10&utm_medium=partners&utm_source=linkshare&utm_campaign=JVFxdTr9V80"
+    }
+];
+
+const GitTutorials = [
+    {
+        key: 123435,
+        img: Magic,
+        title: "Git Magic",
+        content: "Great resource for all things Git. Git is a version control Swiss army knife. A reliable versatile multipurpose revision control tool whose extraordinary flexibility makes it tricky to learn, however extemely important.",
+        btnSrc: "http://www-cs-students.stanford.edu/~blynn/gitmagic/"
+        
+    },
+    {
+        key: 3342234343454,
+        img: GitLogo,
+        title: "Version Control with Git",
+        content: "Have you ever worked on a project that was so unwieldy, you were scared to update a file or add a feature? Maybe the problem was that you weren't using a version control system. ",
+        btnSrc: "https://code.tutsplus.com/tutorials/easy-version-control-with-git--net-7449"
+    },
+    {
+        key: 234235443,
+        img: Hubspot,
+        title: "Intro to Git and Github",
+        content: "Step by step guide to mastering git with github. Get comfortable making changes to the code base, opening up a pull request, and merging code into the master branch",
+        btnSrc: "https://product.hubspot.com/blog/git-and-github-tutorial-for-beginners"
+    }
+];
+
+export {GitResources, GitTutorials};
