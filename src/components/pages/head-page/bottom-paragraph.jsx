2fd71998221a5e05eb76dcee694731e296a1a21b
Damon Roberts <66637938+damonspencerroberts@users.noreply.github.com>
2020-09-26 01:49:44 -0400

    remove news will work out later

--- src/components/pages/head-page/bottom-paragraph.jsx
+++ src/components/pages/head-page/bottom-paragraph.jsx
@@ -3,7 +3,6 @@ import News from "./econ-new";
 
 const bottomPara = () => <div className = "head-bottom-para">
         <hr />
-        <News />
         <div className = "head-bp">
             <p>We hope you enjoy this site, and please do not hesitate to contact us if you have any questions.</p>
         </div>
